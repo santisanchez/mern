719ec2b8dc3d082571e567ff2431a239e33ffd86
Santiago <santisanchez.1214@gmail.com>
2020-02-09 08:49:53 -0500

    Se crea modulo inicial para mostrar a los jugadores

--- client/src/components/organisms/PlayerModule.js
+++ client/src/components/organisms/PlayerModule.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import './PlayerModule.scss';
 
-export default function PlayerModule() {
-    return (
-        <div>
-            Player
-        </div>
-    )
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 120,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function PlayerModule({player}) {
+    const classes = useStyles();
+    return (<Paper className={classes.paper}>
+        <span>{player.name}</span>
+      </Paper>
+    );
 }
